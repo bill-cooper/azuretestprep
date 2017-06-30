aaa61070bc30b3d059599d8eb924e88496593abd
bill-cooper <william.l.cooper1@gmail.com>
2017-06-30 17:01:40 -0500

    added QueueProcessorWebJob project

---
+++ azuretestprep/QueueProcessorWebJob/Functions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.WebJobs;
+using System.Net;
+using System.IO;
+using ImageProcessor;
+using System.Drawing;
+using ImageProcessor.Imaging.Formats;
+
+namespace QueueProcessorWebJob
+{
+    public class Functions
+    {
+        public static void DownloadImage([QueueTrigger("imageurl")] string imageUrl, IBinder binder)
+        {
+            var fileName = Path.GetFileName(imageUrl);
+            var writer = binder.Bind<Stream>(new BlobAttribute($"images/{fileName}", FileAccess.Write));
+            
+            var image = new WebClient().DownloadData(imageUrl);
+            writer.Write(image,0,image.Length);
+        }
+
+        public static void Create240x([BlobTrigger("images/{name}.{ext}")] Stream input, string name, string ext, [Blob("images-240/{name}_240x.{ext}", FileAccess.Write)] Stream output, TextWriter log)
+        {
+            log.WriteLine($"Resizing {name}.{ext} to 240x");
+            Resize(240, input, output);
+        }
+
+        private static void Resize(int width, Stream input, Stream output)
+        {
+            using (var factory = new ImageFactory(preserveExifData: true))
+            using (var memory = new MemoryStream())
+            {
+                factory.Load(input)
+                  .Resize(new Size(width, 0))
+                  .Format(new JpegFormat { Quality = 75 })
+                  .Save(memory);
+
+                memory.CopyTo(output);
+            }
+        }
+    }
+}
---
+++ azuretestprep/QueueProcessorWebJob/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.WebJobs;
+
+namespace QueueProcessorWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new JobHost();
+            host.RunAndBlock();
+        }
+    }
+}
